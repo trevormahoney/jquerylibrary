70cc3bd39b7b68076dcfa5036c0acf5894d01fa7
trevormahoney <trevormahoney12345@gmail.com>
2017-01-18 16:57:43 -0500

    first commit

---
+++ 0.11_module_11_manipulatedomchallenge.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	var content ="<p>Stuff I added!</p>";
+	$("#divText").append(content)
+
+	$("styled").css("color", "green")
+
+	$("#adding-class").css("border", "2px solid red")
+})
+
